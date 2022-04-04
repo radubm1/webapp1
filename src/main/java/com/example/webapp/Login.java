e9922b8e3d1f7a362ce347967e88320d2eae3459
RADU <radub_m@yahoo.com>
2022-04-04 12:20:06 +0300

    Orig1

--- src/main/java/com/example/webapp/Login.java
+++ src/main/java/com/example/webapp/Login.java
@@ -1,10 +1,7 @@
 package com.example.webapp;
 
 
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import java.sql.*;
 import java.util.Properties;
 
@@ -12,25 +9,27 @@ import java.util.Properties;
 public class Login {
     @POST
     @Produces("text/plain")
-    public String login(@QueryParam("usr") String usr, @QueryParam("pwd") String pwd) {
-        String msg="Trying to connect...";
+    public String login(@FormParam("usr") String usr, @FormParam("pwd") String pwd) {
+        StringBuilder msg= new StringBuilder("Trying to connect...");
+        //msg.append("using:"); msg.append(usr); msg.append(" and "); msg.append(pwd);
         try {
             String url = "jdbc:postgresql://localhost:5432/postgres";
             Properties props = new Properties();
             props.setProperty("user","postgres");
             props.setProperty("password","admin");
             //props.setProperty("ssl","false");
+            Class.forName("org.postgresql.Driver");
             Connection conn = DriverManager.getConnection(url, props);
             PreparedStatement st = conn.prepareStatement("SELECT * FROM users WHERE name = ? and password = ?");
             st.setString(1,usr);
             st.setString(2,pwd);
             ResultSet rs = st.executeQuery();
             while (rs.next()) {
-                msg="Hello, " + rs.getString(2) + "! You've successfully logged in!";
+                msg.append("Hello, " + rs.getString(2) + "! You've successfully logged in!");
             }
-            rs.close();
-            st.close();
-        } catch (SQLException e) {System.out.println(e.getMessage());}
-        return msg;
+        } catch (SQLException e) {System.out.println(e.getMessage());} catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return msg.toString();
     }
 }
